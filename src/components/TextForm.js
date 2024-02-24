5ff414f197771d942d67f281c8af3a4b7b3ff02a
ashish <1508ramashish@gmail.com>
2024-02-24 21:38:27 +0530

    cssdisplay

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -54,11 +54,13 @@ export default function TextForm(props) {
             <div className="mb-3"> 
             <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
             </div>
+            <div style={{display:'flex',flexWrap:'wrap',justifyContent:'center',gap:'1vw'}}>
             <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
             <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
             <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
             <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+            </div>
         </div>
         <div className="container my-3" style={{color:'gray'}}>
             <div className="container"> <h2>Your text summary</h2> 
